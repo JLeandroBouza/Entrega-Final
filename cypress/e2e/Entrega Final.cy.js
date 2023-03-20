857202c114d6a69d1130c8dc2b06c920c2654443
JLeandroBouza <jorge.leandrobouza@gmail.com>
2023-03-20 18:51:46 -0300

    Correcciones Marcadas con respectos a las Verificaciones en Test y Creacion de nueva Rama para corregir

--- cypress/e2e/Entrega Final.cy.js
+++ cypress/e2e/Entrega Final.cy.js
@@ -23,19 +23,22 @@ describe("Entrega Final: Pre Entrega", () =>{
     })
 
     it("Prueba del Modulo Online Shop", () =>{
+
+        const sumTotal= `${InputData.product.price1 + InputData.product.price2}`
+
         cy.visit(" ");
+
         registerPage.clickLogIn();
         loginPage.login(InputData.login.username, InputData.login.password);
         homePage.clickOnlineShop();
         productsPage.addproducts(InputData.product.productName1);
         productsPage.addproducts(InputData.product.productName2);
         productsPage.clickGoShoppingCart();
-        shoppingCartPage.verifyProductName(InputData.product.productName1);
-        shoppingCartPage.verifyProductName(InputData.product.productName2);
-        shoppingCartPage.verifyProductPrice(InputData.product.productName1,InputData.product.price1);
-        shoppingCartPage.verifyProductPrice(InputData.product.productName2,InputData.product.price2);
-        shoppingCartPage.clickShowTotalPrice();
-        shoppingCartPage.verifyTotalPrice(InputData.product.price1,InputData.product.price2);
+        shoppingCartPage.verifyProductName(InputData.product.productName1).should("have.text",`${InputData.product.productName1}`);
+        shoppingCartPage.verifyProductName(InputData.product.productName2).should("have.text",`${InputData.product.productName2}`);
+        shoppingCartPage.verifyProductPrice(InputData.product.productName1,InputData.product.price1).should("have.text",`$${InputData.product.price1}`);
+        shoppingCartPage.verifyProductPrice(InputData.product.productName2,InputData.product.price2).should("have.text",`$${InputData.product.price2}`);
+        shoppingCartPage.verifyTotalPrice(InputData.product.price1,InputData.product.price2).should("have.text", sumTotal);
     });
 
 })
